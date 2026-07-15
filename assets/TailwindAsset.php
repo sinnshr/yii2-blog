4f839456cd0bffab786d0502f9bb5e887709a240
sinshr <98868456+sinnshr@users.noreply.github.com>
2026-07-16 00:49:34 +0330

    Add authManager configuration and update database connection settings

--- assets/TailwindAsset.php
+++ assets/TailwindAsset.php
@@ -1,4 +1,5 @@
 <?php
+namespace app\assets;
 
 class TailwindAsset extends \yii\web\AssetBundle
 {
--- config/web.php
+++ config/web.php
@@ -12,6 +12,9 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'lqqFgpisXgpZtu9d2XT96AjL8sk00p6l',
